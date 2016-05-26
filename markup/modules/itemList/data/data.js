98770b9ffc511cb293dc1bf14ff0a3e8e3619126
vafarulez <vafarulez@ya.ru>
2016-05-26 12:15:26 +0300

    адаптивность

---
+++ markup/modules/itemList/data/data.js
@@ -0,0 +1,51 @@
+itemList: [
+	{
+		imageUrl: '/static/img/content/thumbs/01.jpg',
+		alt: 'Какое то описание'
+	},
+	{
+		imageUrl: '/static/img/content/thumbs/02.jpg',
+		alt: 'Какое то описание'
+	},
+	{
+		imageUrl: '/static/img/content/thumbs/03.jpg',
+		alt: 'Какое то описание'
+	},
+	{
+		imageUrl: '/static/img/content/thumbs/04.jpg',
+		alt: 'Какое то описание'
+	},
+	{
+		imageUrl: '/static/img/content/thumbs/05.jpg',
+		alt: 'Какое то описание'
+	},
+	{
+		imageUrl: '/static/img/content/thumbs/06.jpg',
+		alt: 'Какое то описание'
+	},
+	{
+		imageUrl: '/static/img/content/thumbs/07.jpg',
+		alt: 'Какое то описание'
+	},
+	{
+		imageUrl: '/static/img/content/thumbs/08.jpg',
+		alt: 'Какое то описание'
+	},
+	{
+		imageUrl: '/static/img/content/thumbs/09.jpg',
+		alt: 'Какое то описание'
+	},
+	{
+		imageUrl: '/static/img/content/thumbs/10.jpg',
+		alt: 'Какое то описание'
+	},
+	{
+		imageUrl: '/static/img/content/thumbs/11.jpg',
+		alt: 'Какое то описание'
+	},
+	{
+		imageUrl: '/static/img/content/thumbs/12.jpg',
+		alt: 'Какое то описание'
+	}
+
+]
